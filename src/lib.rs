2c3ad3583548c46943c9994f43960f0b68100ca6
Steve Walton <37951256+SteveBWalton@users.noreply.github.com>
2022-12-25 13:22:46 +0000

    Added a very simple person structure.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// Attempt create a library that will expose a family tree object library from a gedcom file.
+
+mod person;
+
+// Member variables for the FamilyTree class.
+pub struct FamilyTree {
+    pub people: Vec<person::Person>
 }
+
+// Methods for the FamilyTree class.
+impl FamilyTree {
+    // Create a new 'FamilyTree' structure.
+    pub fn new() -> FamilyTree {
+        let mut people = Vec::new();
+
+        let my_person = person::Person{first_name: "Steve".to_string(), last_name: "Walton".to_string()};
+        people.push(my_person);
+
+        FamilyTree{people: people}
+    }
+
+    // Open a gedcom file and create objects from it.
+    pub fn open(&mut self, file_name: &str) {
+    }
+}
+
+
 
 #[cfg(test)]
 mod tests {
@@ -8,7 +32,8 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let mut result = FamilyTree::new();
+        result.open("test_file.gedcom");
+        assert_eq!(result.people.len(), 1);
     }
 }
---
+++ src/person.rs
@@ -0,0 +1,8 @@
+// Module to define the 'Person' class.
+// Rust does have obejcts!
+
+// Members variables for the 'Person' class.
+pub struct Person {
+    pub first_name: String,
+    pub last_name: String,
+}
